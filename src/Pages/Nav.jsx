4b2d30c53e47f6c608978d74e0eddc117a0ca482
bilgates27 <bilgates270703@gmail.com>
2024-06-22 14:18:43 +0530

    Home Post committed

--- src/Pages/Nav.jsx
+++ src/Pages/Nav.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState, useRef } from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { UserContext } from '../context/UserContext';
 import 'boxicons/css/boxicons.min.css';
@@ -6,29 +6,10 @@ import Account from '../componant/Account';
 
 const Nav = () => {
   const { user } = useContext(UserContext);
-  const [scrollingUp, setScrollingUp] = useState(true);
-  const lastScrollTop = useRef(0);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollTop = document.documentElement.scrollTop;
-      if (scrollTop > lastScrollTop.current) {
-        // Scroll down
-        setScrollingUp(false);
-      } else {
-        // Scroll up
-        setScrollingUp(true);
-      }
-      lastScrollTop.current = scrollTop;
-    };
-
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
 
   return (
     user && (
-      <nav className={`nav ${scrollingUp ? '' : 'hidden'}`}>
+      <nav className="nav">
         <ul className="nav-list">
           <li className="nav-item">
             <Link to="/Home" className="nav-link">
